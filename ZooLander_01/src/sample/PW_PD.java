bbe482c114f12a98a6fd0f76aec1aa6d9211f023
ManassehBBTNA <manasseh.boyd@nationalarchives.gsi.gov.uk>
2018-01-24 23:32:22 +0000

    Update PW_PD.java
    added method for calculating pen size

--- ZooLander_01/src/sample/PW_PD.java
+++ ZooLander_01/src/sample/PW_PD.java
@@ -9,11 +9,17 @@ public class PW_PD extends Pen {
 
     private double PD_length;
     private double PD_width;
+    private double PW_Size;
+    private double PD_Size;
+    private double PW_penSize;
+    private double PD_penSize;
+
 
     private Animal[] Animals;
     private Keeper [] Keepers;
 
-    public void PW_PD (String Type, double PW_length, double PW_width, double PW_height, double PD_length, double PD_width, Animal[] Animals, Keeper[] Keepers) {
+    public void PW_PD (String Type, double PW_length, double PW_width, double PW_height,
+                       double PD_length, double PD_width, Animal[] Animals, Keeper[] Keepers) {
         this.Type = Type;
         this.PW_length = PW_length;
         this.PW_width = PW_width;
@@ -26,25 +32,26 @@ public class PW_PD extends Pen {
         return Type;
     }
 
-    public double getPW_length(){
-        return PW_length;
-    }
+    public double getPW_length(){return PW_length;}
 
     public double getPW_width() { return PW_width;}
 
     public double getPW_height() { return PW_height;}
 
-    public double getPD_length(){
-        return PD_length;
-    }
+    public double getPD_length(){return PD_length;}
 
     public double getPD_width() { return PD_width;}
 
-    public Animal[] getAnimals(){
-        return Animals;
-    }
+    public Animal[] getAnimals(){return Animals;}
 
-    public Keeper[] getKeepers(){
-        return Keepers;
+    public Keeper[] getKeepers(){return Keepers;}
+
+    //method to calculate the overall size of the PW_PD Pen
+    @Override
+    public void PenSize (){
+        PW_Size = PW_length * PW_width * PW_height;
+        PD_Size = PD_length * PD_width;
+        PW_penSize = PW_Size;
+        PD_penSize = PD_Size;
     }
 }
